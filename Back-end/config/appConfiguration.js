d7af680a3812988a77a4c491955e0191495d7052
Rinkesh Patel <rinkeshp@cybage.com>
2019-08-29 19:27:31 +0530

    Create appConfiguration.js

---
+++ Back-end/config/appConfiguration.js
@@ -0,0 +1,21 @@
+let appConfig = {};
+
+appConfig.port = 3002;
+appConfig.allowedCorsOrigin = "*";
+appConfig.env = "dev";
+appConfig.db = {
+  uri: 'mongodb://127.0.0.1:27017/issueTrackerDB1'
+}
+appConfig.apiVersion = '/api/v1';
+appConfig.emailId = '';
+appConfig.password = '';
+
+module.exports = {
+  port: appConfig.port,
+  allowedCorsOrigin: appConfig.allowedCorsOrigin,
+  environment: appConfig.env,
+  db: appConfig.db,
+  apiVersion: appConfig.apiVersion,
+  emailId: appConfig.emailId,
+  password: appConfig.password
+};
